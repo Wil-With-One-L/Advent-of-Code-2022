3ca4a11ec32377f8b46f575e810647c8e0557557
Wil-With-One-L <99309050+Wil-With-One-L@users.noreply.github.com>
2023-05-24 13:23:52 -0500

    Day 2 Puzzle 2 Solved

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -13,23 +13,23 @@ A = dict() # rock
 B = dict() # paper
 C = dict() # scissors
 
-A['X'] = 3
-A['Y'] = 6
-A['Z'] = 0
+A['X'] = 0 + 3
+A['Y'] = 3 + 1
+A['Z'] = 6 + 2
 
-B['X'] = 0
-B['Y'] = 3
-B['Z'] = 6
+B['X'] = 0 + 1
+B['Y'] = 3 + 2
+B['Z'] = 6 + 3
 
-C['X'] = 6
-C['Y'] = 0
-C['Z'] = 3
+C['X'] = 0 + 2
+C['Y'] = 3 + 3
+C['Z'] = 6 + 1
 
-points = dict()
+# points = dict()
 
-points['X'] = 1
-points['Y'] = 2
-points['Z'] = 3
+# points['X'] = 1
+# points['Y'] = 2
+# points['Z'] = 3
 
 total = 0
 for round in rounds:
@@ -42,6 +42,6 @@ for round in rounds:
     sum += B[resp]
   elif opp == 'C':
     sum += C[resp]
-  total += sum + points[resp]
+  total += sum
 
 print(total)
