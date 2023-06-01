1a84ca1b69f2f2a5c4c6b00c0d92bc0d85671813
Wil-With-One-L <99309050+Wil-With-One-L@users.noreply.github.com>
2023-06-01 16:14:45 -0500

    Solve Day 18 Puzzle 2

--- Day 18/day18.py
+++ Day 18/day18.py
@@ -7,50 +7,82 @@ def cleanInput(filename):
       lines[i][j] = int(lines[i][j])
   return lines
 
-def getNeighbors(cube):
+def getNeighbors(cube, size):
   neighbors = []
-  neighbors.append([cube[0] + 1, cube[1], cube[2]])
-  neighbors.append([cube[0] - 1, cube[1], cube[2]])
-  neighbors.append([cube[0], cube[1] + 1, cube[2]])
-  neighbors.append([cube[0], cube[1] - 1, cube[2]])
-  neighbors.append([cube[0], cube[1], cube[2] + 1])
-  neighbors.append([cube[0], cube[1], cube[2] - 1])
+  if cube[0] < size - 1:
+    neighbors.append([cube[0] + 1, cube[1], cube[2]])
+  if cube[0] > 0:
+    neighbors.append([cube[0] - 1, cube[1], cube[2]])
+  if cube[1] < size - 1:
+    neighbors.append([cube[0], cube[1] + 1, cube[2]])
+  if cube[1] > 0:
+    neighbors.append([cube[0], cube[1] - 1, cube[2]])
+  if cube[2] < size - 1:
+    neighbors.append([cube[0], cube[1], cube[2] + 1])
+  if cube[2] > 0:
+    neighbors.append([cube[0], cube[1], cube[2] - 1])
   return neighbors
 
+def getExterior(cubes, size):
+  exteriors = []
 
-# def make3DGrid(size):
-#   grid = []
-#   for x in range(0, size):
-#     row = []
-#     for y in range(0, size):
-#       col = []
-#       for z in range(0, size):
-#         col.append(0)
-#       row.append(col)
-#     grid.append(row)
-#   return grid
+  # BFS babEEEYYYYY
+  start = [0,0,0]
+  visited = []
+  queue = []
+
+  queue.append(start)
+    
+  while len(queue) > 0:
+    # get next non-visited cell
+    while queue[0] in visited:
+      queue.pop(0)
+      if len(queue) == 0:
+        break
+    if len(queue) == 0:
+      break
+  
+    cube = queue[0]  
+    
+    # add all EMPTY neighbors to queue:
+    neighbors = getNeighbors(cube, size)
+    for n in neighbors:
+      if (n not in cubes) and (n not in visited):
+        queue.append(n)
+  
+    exteriors.append(cube)
+    visited.append(cube)
+    queue.pop(0)
+  
+  return exteriors
 
 # =======================
 
-grid_size = 19
+size = 22
 # grid = make3DGrid(grid_size)
 
 # =======================
 
 cubes = cleanInput('Day 18/input.txt')
-print(cubes)
+# print(cubes)
+
+# =======================
+
+exterior_cubes = getExterior(cubes, size)
 
 total = 0
 
 for cube in cubes:
-  neighbors = getNeighbors(cube)
+  neighbors = getNeighbors(cube, size)
   sides = 6
   for neighbor in neighbors:
-    if neighbor in cubes:
+    if neighbor in cubes or neighbor not in exterior_cubes:
       sides -= 1
   total += sides
       
 print(total)
 
-# 3338 too high
-# 3320 too low
+# 1905 too low
+# 1958 too low
+
+# 1996!
